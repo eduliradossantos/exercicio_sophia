f38ab1a084fda1def0e74d40ba7647f2d5e023cd
Eduardo Lira <81393461+eduliradossantos@users.noreply.github.com>
2025-09-04 20:26:43 +0000

    segundo commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("SofIA - Introdução ao Streamlit")
+
+st.write("Bem-vindos à nossa aula sobre **Streamlit**: criando interfaces rápidas em Python!")
+
+st.sidebar.title("Menu Lateral")
+
+pagina = st.sidebar.radio("Escolha:",["Cadastro", "Upload","Gráficos"])
+
+if pagina == "Cadastro":
+    st.write("Você está na página inicial")
+    st.title("Widgets")
+    nome = st.text_input("Digite seu nome:")
+
+elif pagina == "Upload":
+    st.write("Informações sobre o app.")
+    st.title("Upload de Arquivos")
+    arquivo = st.file_uploader("Envie um arquivo CSV")
+    if arquivo:
+        df = pd.read_csv(arquivo)
+        st.dataframe(df)
+
+else:
+    dados = pd.DataFrame({
+        "Alunos":["Ana","Bruno","Carlos","Diana"],
+        "Notas":[8.5, 7.0, 9.2, 6.8]
+    })
+    st.bar_chart(dados.set_index("Alunos"))
